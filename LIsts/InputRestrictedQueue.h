8716bdfefee34d9ab36339de3bfdc758e861f1d7
cnnbless <antongolubchuk@gmail.com>
2024-04-22 22:32:14 +0300

    Added template functions (Enqueue, Dequeue, DeleteFromEnd, Peek, PeekFromEnd, IsEmpty, getSize)

---
+++ LIsts/InputRestrictedQueue.h
@@ -0,0 +1,110 @@
+//
+// Created by Tony on 09.04.2024.
+//
+
+#ifndef DATA_STRUCTURES_INPUTRESTRICTEDQUEUE_H
+#define DATA_STRUCTURES_INPUTRESTRICTEDQUEUE_H
+
+
+
+#include "../node/QueueNode.h"
+#include <iostream>
+using namespace std;
+using namespace queueNode;
+
+template<typename T>
+class InputRestrictedQueue {
+private:
+    shared_ptr<QueueNode<T>> head;
+    shared_ptr<QueueNode<T>> tail;
+    int size;
+
+public:
+    void Enqueue(T data) {
+        shared_ptr<QueueNode<T>> newNode = make_shared<QueueNode<T>>(data);
+        if (!tail) {
+            head = tail = newNode;
+        }
+        else {
+            tail->next = newNode;
+            newNode->previous = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    T Dequeue() {
+        if (!head) {
+            throw runtime_error("Queue is empty");
+        }
+
+        T temp = head->data;
+        if (head == tail) {
+            head = tail = nullptr;
+        }
+        else {
+            head = head->next;
+            head->previous.reset();
+        }
+        size--;
+        return temp;
+    }
+    T DeleteFromEnd() {
+        if (!tail) {
+            throw runtime_error("Queue is empty");;
+        }
+
+        T temp = tail->data;
+        if (head == tail) {
+            head = tail = nullptr;
+        }
+        else {
+            tail = tail->previous.lock();
+            tail->next = nullptr;
+        }
+        size--;
+        return temp;
+    }
+
+    T Peek() {
+        if (!head) {
+            throw runtime_error("Queue is empty");
+        }
+        return head->data;
+    }
+    T PeekFromEnd(){
+        if(!tail){
+            throw runtime_error("Queue is empty");
+        }
+        return tail->data;
+    }
+
+    bool IsEmpty(){
+        if(size==0){
+            return true;
+        }
+        return false;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    friend ostream &operator<<(ostream &os, InputRestrictedQueue &obj) {
+        os << endl;
+        shared_ptr<QueueNode<T>> current = obj.head;
+        while (current) {
+            os << current->data << " ";
+            current = current->next;
+        }
+        os << endl;
+        return os;
+    }
+
+    InputRestrictedQueue(): head{nullptr}, tail{nullptr}, size{0} {};
+    InputRestrictedQueue(T data) : head{make_shared<QueueNode<T>>(data)}, tail{head}, size{1} {};
+    ~InputRestrictedQueue() {};
+};
+
+
+#endif //DATA_STRUCTURES_INPUTRESTRICTEDQUEUE_HRICTEDQUEUE_H
